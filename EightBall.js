715270b9375a9139e9943bb3270993beac321cde
Karol Nowosad <drogakrzyzowa@gmail.com>
2019-01-02 19:52:24 +0000

    Upgrade of EightBall and another project: Race Day

--- EightBall.js
+++ EightBall.js
@@ -1,9 +1,9 @@
-You've learned a powerful tool in JavaScript: control flow! It's 
+/*You've learned a powerful tool in JavaScript: control flow! It's 
 so powerful, in fact, that it can be used to tell someone's fortune.
 
 In this project we will build the Magic Eight Ball using control flow in JavaScript.
 
-The user will be able to input a question, then our program will output a random fortune.
+The user will be able to input a question, then our program will output a random fortune. */
 
 let userName = 'Karol';
 
